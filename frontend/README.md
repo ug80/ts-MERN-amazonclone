c77086c78cdd28d9c02cc26fdf2eed3244b285f1
Ulises Giordano <giordau@gmail.com>
2024-04-23 19:59:51 -0300

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# Typescript MERN AMAZONCLONE
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TS AMAZON-CLONE</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
