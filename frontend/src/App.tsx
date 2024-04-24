e5fb9022083d51f4cc0b9b7290a3a2cbb1500a07
Ulises Giordano <giordau@gmail.com>
2024-04-23 21:22:22 -0300

    List sample products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { sampleProducts } from './Data'
 
 function App() {
   return (
     <div>
       <header>TS AMAZON-CLONE</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
