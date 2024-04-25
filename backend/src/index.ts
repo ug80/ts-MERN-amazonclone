702e07793f43cb8dc60457b4a736453e403c435f
Ulises Giordano <giordau@gmail.com>
2024-04-24 21:52:40 -0300

    fetch data

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+import { sampleProducts } from './Data'
+import cors from 'cors'
+
+const app = express()
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:5173'],
+  })
+)
+app.get('/api/products', (req: Request, res: Response) => {
+  res.json(sampleProducts)
+})
+const PORT = 4000
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`)
+})
